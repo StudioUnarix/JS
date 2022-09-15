de434c6181b8e693c747febf41695e8386d774d6
StudioUnarix <Un.unarix@gmail.com>
2022-09-15 12:31:27 +0200

    15/09/2022 v1.0.0.3

--- script.js
+++ script.js
@@ -39,3 +39,24 @@ let voisin = ["calvin" , "Jeremy" , "Quentin"];
 voisin.unshift("David");
 voisin.push("Aurelie");
 
+
+let tricolor = ["rouge" , "orange" , "vert" , "en panne"];
+
+couleur = console.prompt("De quel couleur est le feu");
+
+if(couleur = "rouge")
+{
+  alert("Vous ne pouvez pas passé");
+}
+else if(couleur = "orange")
+{
+    alert("Préparer vous à vous arrêté");
+}
+else if(couleur = "vert")
+{
+ alert("vous pouvez passé");
+}
+else
+{
+    alert("En panne faite attention");
+}
