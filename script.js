43bfa74c0431381bd34b9da8688b7246f3bd5644
StudioUnarix <Un.unarix@gmail.com>
2022-09-15 11:34:21 +0200

    15/09/2022 v1.0.0.1

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ let Nomvoisin = "calvin";
 let PrenomVoisin = "Yardin";
 let Annepro = age + 1;
 
+let florian = {
+    Nom: "Larochette" ,
+    Prenom: "Florain" ,
+    age: "20" ,
+    genre: "Tractopelle U25 6 roues"
+};
+
 document.querySelector(".prenom").innerText = prenom
 document.querySelector(".nom").innerText = Nom
 document.querySelector(".age").innerText = age
