486c61bbf4132da2d91d1107b70cdb1ac6582364
StudioUnarix <Un.unarix@gmail.com>
2022-09-15 11:27:18 +0200

    15/09/2022

---
+++ script.js
@@ -0,0 +1,13 @@
+let Nom = "larochette";
+let prenom = "florian";
+let age = 20;
+let Nomvoisin = "calvin";
+let PrenomVoisin = "Yardin";
+let Annepro = age + 1;
+
+document.querySelector(".prenom").innerText = prenom
+document.querySelector(".nom").innerText = Nom
+document.querySelector(".age").innerText = age
+document.querySelector(".Annepro").innerText = Annepro
+document.querySelector(".Nomvoisin").innerText = Nomvoisin
+document.querySelector(".PrenomVoisin").innerText = PrenomVoisin
